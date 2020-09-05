f5d910ee4944a3a64f30d04886e5b1a933e9802c
afazeez <afazeez82@gmail.com>
2020-09-05 15:57:48 -0500

    updated the backgroung picture and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 /*!
     Title: Dev Portfolio Template
     Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
+    Last Change: 09/03/2020
+    Author: Anees Azeez
+
 
     Description: This file contains all the scripts associated with the single-page
     portfolio website.
